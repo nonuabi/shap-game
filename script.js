be25870edc4387190de58fbb96d7dedbd910c6aa
nonuabi <nonuabi@gmail.com>
2021-03-22 13:32:48 +0530

    color change is created and it's javascript part is complete

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strick'
+var colorbtn = document.getElementById('btnc');
+var circle = document.getElementById('circle');
+
+var count = 1;
+function changeColor() {
+	if(count == 1) {
+		circle.style.backgroundColor = "yellow";
+	}else if(count==2) {
+		circle.style.backgroundColor = "blue";
+	} else if (count == 3) {
+		circle.style.backgroundColor = "purple";
+	}else if(count == 4) {
+		count =0;
+	}
+	count++;
+}
+
+colorbtn.addEventListener('click', changeColor);
+
+
+//change shapes
+var shape = document.getElementById('shape');
+var shapebtn = document.getElementById('btns');
+
+function triangle() {
+	
+}
+
+
+
+shapebtn.addEventListener('click', triangle);
